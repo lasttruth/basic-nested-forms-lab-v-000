8f766ac8edab2a921d65ec21b3c15aff314f9a9a
Robin Doirin <robindoirin@gmail.com>
2018-07-12 00:58:33 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,51 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to @recipe
   end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(
+        :title,
+        ingredients_attributes: [
+          :name,
+          :quantity
+          ]
+        )
+    enddef show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to @recipe
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(
+        :title,
+        ingredients_attributes: [
+          :name,
+          :quantity
+          ]
+        )
+    end
 end
